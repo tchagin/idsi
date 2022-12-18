6ca3c83214f223026abb589f90204e6b2fb24986
Vercetty <tchagin.valera@yandex.com>
2022-12-19 04:08:07 +0500

    add new files

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -7,6 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
+/**
+ * App\Models\Application
+ *
+ * @property int $id
+ * @property string $theme
+ * @property string $message
+ * @property string|null $file
+ * @property int $user_id
+ * @property string $status
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User|null $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Application newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Application newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Application query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Application whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Application whereFile($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Application whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Application whereMessage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Application whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Application whereTheme($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Application whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Application whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Application extends Model
 {
     use HasFactory;
